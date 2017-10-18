486a5c8b0a30f41bb674d86e8c72faba0ff37867
lizhiming <liwiseap@163.com>
2017-10-18 11:27:17 +0800

    second commit

---
+++ src/main/java/com/osc/oscashapp/OscashAppApplication.java
@@ -0,0 +1,12 @@
+package com.osc.oscashapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OscashAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OscashAppApplication.class, args);
+	}
+}
